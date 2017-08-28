bb7167755ec66241bbb3ddec0ffb7ca12ce4565a
Shane Laymance <shane.laymance@gmail.com>
2017-08-28 14:58:55 -0700

    implement beesbeesbees using ES6'

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,17 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super();
+    this.age = 40;
+    this.color = 'grey';
+    this.job = 'gamble';
+    this.canFly = false;
+  }
+
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
